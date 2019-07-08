bb9d2ca8fd1589b06e4159406d543fa125f6a37f
joshua255 <28415851+joshua255@users.noreply.github.com>
2019-07-08 08:18:54 +0300

    remove header files, consolidate declarations

--- ursa.ino
+++ ursa.ino
@@ -1,7 +1,85 @@
-#include "ursa.h"
-#include "mpu.h"
-#include "stepper.h"
-#include "wifi.h"
+#include <PID_v1.h>
+#include <Wire.h>  // scl=22 sda=21
+#include "driver/rmt.h"
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WiFiUdp.h>
+
+#define ROBOT_ID 0  // unique robot ID, sent to DS, and used to name wifi network
+#define MODEL_NO 0  // unique configuration of robot which can be used to identify additional features
+#define WiFiLossDisableIntervalMillis 100  // if no data packet has been recieved for this number of milliseconds, the robot disables to prevent running away
+float COMPLEMENTARY_FILTER_CONSTANT = .9997;  // higher = more gyro based, lower=more accelerometer based
+int MAX_SPEED = 4000;  // max speed (in steps/sec) that the motors can run at
+float MAX_TIP = 60;  // max angle in degrees the robot will attempt to recover from -- if passed, robot will disable
+
+// The following lines define STEP pins and DIR pins. STEP pins are used to
+// trigger a step (when rides from LOW to HIGH) whereas DIR pins are used to
+// change the direction at which the motor is driven.
+#define LEFT_STEP_PIN GPIO_NUM_32
+#define LEFT_DIR_PIN GPIO_NUM_33
+#define RIGHT_STEP_PIN GPIO_NUM_34
+#define RIGHT_DIR_PIN GPIO_NUM_35
+#define ENS_PIN GPIO_NUM_23  // pin wired to both motor driver chips' ENable pins, to turn on and off motors
+#define LED_BUILTIN 2
+
+#define movementThreshold 34
+#define movementMeasurements 15
+
+#define maxWifiRecvBufSize 50  // max number of bytes to receive
+#define maxWifiSendBufSize 50  // max number of bytes to send
+
+byte voltage = 0;  // 0v=0 13v=255
+
+// since multiple tasks are running at once, we don't want two tasks to try and use one array at the same time.
+SemaphoreHandle_t mutexReceive;  // used to check whether receiving tasks can safely change shared variables
+
+boolean robotEnabled = false;     // enable outputs?
+boolean wasRobotEnabled = false;  // to know if robotEnabled has changed
+boolean enable = false;           // is the DS telling the robot to enable? (different from robotEnabled so the robot can disable when tipped even if the DS is telling it to enable)
+boolean tipped = false;
+
+double targetPitch = 0.000;  // what angle the balencing control loop should aim for the robot to be at, the output of the speed control loop
+volatile int leftMotorSpeed = 0;  // stepper ticks per second that the left motor is currently doing "volatile" because used in an interrupt
+volatile int rightMotorSpeed = 0;
+double motorSpeedVal = 0;  // how much movement in the forwards/backwards direction the motors should move-only one set of control loops is used for balencing, not one for each motor
+double speedVal = 0;  // how many stepper ticks per second the robot should try to drive at-the input to the speed control loop.
+int turnSpeedVal = 0;  // (positive=turn right, negative=turn left)
+
+double kP_angle, kI_angle, kD_angle = 0.0000;  // PID gains for the Angle control loop
+double kP_speed, kI_speed, kD_speed = 0.0000;  // PID gains for the Speed control loop
+
+int16_t accelerationX, accelerationY, accelerationZ, rotationX, rotationY, rotationZ = 0;
+int32_t rotationOffsetX, rotationOffsetY, rotationOffsetZ = 0;  // "offset" values used to zero the MPU6050 gyro on startup
+unsigned long lastCalcedMPU6050 = 0;  // micros() value of last orientation read. used to integrate gyro data to get rotation
+double rotationDPS_X, rotationDPS_Y, rotationDPS_Z = 0.000;  // rotation in Degrees Per Second around the X,Y, and Z axes, with x left right, y forwards and backwards and z up and down
+double pitch = 0.000;  // output (in degrees) from the MPU6050 reading code. negative=forwards, positive=back Pitch matters for self balancing.
+float pitchOffset = 0.000;  // subtracted from the output in readMPU6050 so that zero pitch can correspond to balanced, not that the control loop cares. Because the MPU6050 may not be mounted in the robot perfectly or because the robot's weight might not be perfectly centered, zero may not respond to perfectly balenced.
+
+hw_timer_t *leftStepTimer = NULL;
+hw_timer_t *rightStepTimer = NULL;
+
+rmt_config_t leftConfig;    // settings for RMT pulse for stepper motor
+rmt_item32_t leftItems[1];  // holds definition of pulse for stepper motor
+rmt_config_t rightConfig;
+rmt_item32_t rightItems[1];
+
+volatile boolean rightForwardBl = false;  // was the motor moving forwards last time the interrupt was called
+volatile boolean leftForwardBl = false;
+
+byte numBytesToSend = 0;
+// Define the SSID and password for the robot's access point
+char robotSSID[12];  // defined in the setup method
+const char *robotPass = "sert2521";
+volatile byte recvdData[maxWifiRecvBufSize] = {0};  // array to hold data recieved from DS.
+volatile boolean receivedNewData = false;  // set true when data gotten, set false when parsed
+volatile byte dataToSend[maxWifiSendBufSize] = {0};  // array to hold data to send to DS.
+byte numAuxRecv = 0;  // how many bytes of control data for extra things
+byte auxRecvArray[12] = {0};  // size of numAuxRecv
+byte numSendAux = 0;  // how many bytes of sensor data to send
+byte auxSendArray[12] = {0};  // size of numAuxSend
+unsigned long lastMessageTimeMillis = 0;
+
+WiFiUDP Udp;
 
 PID PIDA(&pitch, &motorSpeedVal, &targetPitch, kP_angle, kI_angle, kD_angle, DIRECT);  // setup the Angle PID loop  PID(&Input, &Output, &Setpoint, Kp, Ki, Kd, Direction)
 PID PIDS(&motorSpeedVal, &targetPitch, &speedVal, kP_speed, kI_angle, kD_angle, DIRECT);  // setup the Speed PID loop
