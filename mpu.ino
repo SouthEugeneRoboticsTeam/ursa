7bfc461ed8bd433514a48e332a0912f628eb08e0
joshua255 <28415851+joshua255@users.noreply.github.com>
2019-08-02 10:15:20 -0700

    comments, and PID value notes
    These values are working well currently, though of course they could be made better:
    PA=0.09074
    IA=0.0
    DA=0.00648
    PS=0.01852
    IS=0.00046
    DS=0.0

--- mpu.ino
+++ mpu.ino
@@ -44,8 +44,8 @@ void readMPU6050() {
   // complementary filter combines gyro and accelerometer tilt data in a way that takes advantage of short term accuracy of the gyro and long term accuracy of the accelerometer
   pitch = COMPLEMENTARY_FILTER_CONSTANT * ((pitch) + rotationDPS_X * (micros() - lastCalcedMPU6050) / 1000000.000)  // add rotation rate as measured by the gyro to current pitch - valid in short term
           + (1 - COMPLEMENTARY_FILTER_CONSTANT) * (degrees(atan2(accelerationY, accelerationZ)) - pitchOffset);   // in the long term drift towards the angle of gravity measured by the accelerometer
-  if (robotEnabled) {
-    pitchOffset = (pitch + pitchOffset) * (1 - PITCH_OFFSET_CHANGE) + pitchOffset * (PITCH_OFFSET_CHANGE);
+  if (robotEnabled) {//only adjust pitchOffset when the robot is enabled
+    pitchOffset = (pitch + pitchOffset) * (1 - PITCH_OFFSET_CHANGE) + pitchOffset * (PITCH_OFFSET_CHANGE);  // slowly move pitchOffset towards the current pitch value, the overall average pitch value should be close to the balance point
   }
   lastCalcedMPU6050 = micros();  // record time of last calculation so we know next time how much time has passed (how much time to integrate rotation rate for)
 }
